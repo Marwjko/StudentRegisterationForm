3036363acf853f042745d94c18b12271bc83314b
Marwjko <131778503+Marwjko@users.noreply.github.com>
2025-10-27 18:33:39 +0300

    Add AssemblyInfo.cs for project metadata
    This file contains assembly attributes for the Student Registration Form project, including title, version, and COM visibility settings.

---
+++ StudentRegisterationForm/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+// File: AssemblyInfo.cs
+// Last Modified: 26/10/2025
+// Description: Part of Student Registration Form Project
+
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+
+[assembly: AssemblyTitle("StudentRegisterationForm")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("StudentRegisterationForm")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("3db5c76e-3a7b-4840-bc3a-4228896c498a")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
